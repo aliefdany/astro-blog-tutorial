e4a9455074117a4c96d5fcac48a4de1dd0840140
aliefdany <aliefseventri@gmail.com>
2024-04-18 11:11:35 +0700

    added astro rss integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://regal-tanuki-5c70fd.netlify.app"
+});
