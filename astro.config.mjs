86f08d4341aa94380b78510bf247c1b0ac397c57
aliefdany <aliefseventri@gmail.com>
2024-04-18 11:21:57 +0700

    added preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://regal-tanuki-5c70fd.netlify.app"
+  site: "https://regal-tanuki-5c70fd.netlify.app",
+  integrations: [preact()]
 });
